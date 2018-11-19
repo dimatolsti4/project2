f31b5cf18464ab31b7635fbecfd77269d5677af0
ADOLF HITLER <44486003+dimatolsti4@users.noreply.github.com>
2018-11-19 22:25:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-crypted_text = input('Введите зашифрованный текст').replace(' ', '').upper()
-while crypted_text.isalpha() == 0:
+crypted_text = input('Введите зашифрованный текст').replace(' ','').upper()
+while (crypted_text.isalpha() == 0) or (len(crypted_text) % 5 != 0):
     print('В строке должны быть только буквы')
-    crypted_text = input('Введите зашифрованный текст').replace(' ', '').upper()
+    print('Количество символов делится на 5')
+    crypted_text = input('Введите зашифрованный текст').replace(' ','').upper()
 binary_code = ''
 uncrypted_text = ''
 letters = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
 
-for letter in range(0, len(crypted_text)):
+for letter in range (0,len(crypted_text)):
     if (64 < ord(crypted_text[letter]) < 91):
         binary_code = binary_code + '0'
     else:
         binary_code = binary_code + '1'
-for letter in range(0, len(crypted_text), 5):
+for letter in range (0,len(crypted_text),5):
     num = binary_code[letter:letter+5]
-    num = int(num, 2)
+    num = int(num,2)
     uncrypted_text = uncrypted_text + letters[num]
 print(uncrypted_text)
